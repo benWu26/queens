823934fd848378a02f9ac4bc5cc915de709935d8
zkhan04 <khanzayd11@gmail.com>
2024-12-21 23:03:28 -0600

    Basic puzzle generation works.
    I'm going to cry tears of joy

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,22 @@ import sampleBoard from './sampleBoard.ts'
 import { solvePuzzle } from './BoardLogic.ts'
 import rfdc from 'rfdc'
 const clone = rfdc();
+import { createGraph, colorGraph, generateBoard } from './BoardLogic.ts'
+
+let validPuzzleGenerated = false;
+let puzzleBoard;
+
+do {
+  puzzleBoard = generateBoard(8);
+    if (solvePuzzle(clone(puzzleBoard)).length === 1){
+      validPuzzleGenerated = true;
+    }
+} while (!validPuzzleGenerated)
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Board board={sampleBoard}></Board>
-    <p>{solvePuzzle(clone(sampleBoard)).length}</p>
+    <Board board={puzzleBoard}></Board>
+    <p>{solvePuzzle(clone(puzzleBoard)).length}</p>
   </StrictMode>,
 )
