ebb2612a2e423022dba4f12dc82852fc9efd0486
zkhan04 <khanzayd11@gmail.com>
2024-12-31 11:47:03 -0600

    fixed the instructions page.

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -9,7 +9,12 @@ function Instructions() {
             <p>
                 No two queens can be in the same row, column, of the same color, or touch diagonally.
             </p>
-            <Board board={sampleBoard} autoPlace={false}></Board>
+            <p>
+                Click a cell once to place a dot, indicating that a queen cannot be placed there.
+            </p>
+            <p>
+                Click again to place a queen.
+            </p>
             <Link to="/play" className="play-link">play</Link>
         </div>
         
