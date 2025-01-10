a9175c39931ffda4d3ed5cf2cc8b5b071e2b5aa9
zkhan04 <khanzayd11@gmail.com>
2025-01-09 21:30:30 -0600

    basic database is set up now. fetching puzzles is REALLY fast when you dont have to generate them lol.
    Next steps: do some refactoring, then find a more efficient way of encoding boards

--- api/src/routes/generate.ts
+++ api/src/routes/generate.ts
@@ -1,9 +1,43 @@
 import express, {Request, Response, Router} from "express";
+import * as dotenv from "dotenv"
 const router: Router = express.Router();
+import { collections } from "../services/database.service";
 
-import { generateValidBoardRuleBased } from "shared";
+dotenv.config();
 
-router.get('/', (req: Request, res: Response) => {
+console.log(process.env.DB_CONNECTION_STRING);
+
+import { boardType, generateValidBoardRuleBased } from "shared";
+
+router.use(express.json())
+
+router.get('/', async (req: Request, res: Response) => {
+    const size = req.query?.size as string | undefined;
+    if (!size) {
+        return res.status(400).json({"message": "please specify the board size."});
+    }
+
+    const boardSize = parseInt(size as string);
+    if (boardSize < 4 || boardSize > 10) {
+        return res.status(400).json({"message": "board size must be between 4 and 10"});
+    }
+
+    const pipeline = [
+        {$match: {size: boardSize}},
+        {$sample: {size: 1}}
+    ]
+
+    const boardDocument = (await collections.boards?.aggregate(pipeline).toArray())
+
+    if (boardDocument?.length) {
+        const boardData = boardDocument[0].board as number[][];
+        res.status(200).json(boardData);
+    } else {
+        res.status(404).json({"message": "board not found"});
+    }
+});
+
+router.post('/', async (req: Request, res: Response) => {
     const size = req.query?.size as string | undefined;
     if (!size) {
         return res.status(400).json({"message": "please specify the board size."});
@@ -15,7 +49,13 @@ router.get('/', (req: Request, res: Response) => {
     }
 
     const board = generateValidBoardRuleBased(boardSize);
+    collections.boards?.insertOne({size: boardSize, board: board});
+
     res.status(200).json(board);
-});
+})
+
+// get: should return a random board of size n from the database
+
+// post: takes the size and color map, adds it to the appropriate collection
 
 export default router;
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,16 @@
 import express, {Express, Request, Response} from "express";
 import generate from "./routes/generate";
+import { connectToDatabase } from "./services/database.service";
 const app: Express = express();
 
 app.use(express.json());
 
-app.use("/generate", generate);
 
-app.listen(3000, () => {
-    console.log("Listening at port 3000!");
-});
+connectToDatabase().then(() => {
+    app.use("/generate", generate);
+
+    app.listen(3000, () => {
+        console.log("Listening at port 3000!");
+    });
+})
+
