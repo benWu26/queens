d4292505d9a22ab5e8a53289823c4e82af1c85a1
zkhan04 <khanzayd11@gmail.com>
2025-01-24 18:32:54 -0600

    Starting migration to serverless framework

--- api/src/middleware/validateBoardSize.ts
+++ api/src/middleware/validateBoardSize.ts
@@ -1,4 +1,6 @@
 import express, {RequestHandler} from "express"
+import type {Context, APIGatewayProxyStructuredResultV2, APIGatewayProxyEvent, Handler} from "aws-lambda";
+
 
 // express middleware for validating the size of a board
 export const validateBoardSize: RequestHandler = (req, res, next) => {
@@ -13,4 +15,22 @@ export const validateBoardSize: RequestHandler = (req, res, next) => {
     }
 
     next();
+}
+
+export const validateBoardSize2: Handler = async (_event: APIGatewayProxyEvent, _context: Context) => {
+    const sizeStr = _event.queryStringParameters?.size as string | undefined;
+    if (!sizeStr) {
+        return {
+            statusCode: 400,
+            body: "please specify the board size"
+        }
+    };
+
+    const boardSize = parseInt(sizeStr as string);
+    if (boardSize < 4 || boardSize > 10) {
+        return {
+            statusCode: 400,
+            body: "board size must be between 4 and 10"
+        }
+    }
 }
